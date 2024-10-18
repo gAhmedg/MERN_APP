3eb6467482c0e6e5200d017ba7dc812ffae8c08e
Ahmed Gomaa <gomaaa447@gmai.com>
2024-10-19 02:33:50 +0300

    update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 const connectionString = 'mongodb://mongo:27017/cinema';
 
-mongoose.connect(connectionString, { useNewUrlParser: true }).catch((e) => {
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).catch((e) => {
   console.error('Connection error', e.message);
-  useNewUrlParser: true;
-  useUnifiedTopology: true;
 });
 
 const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 module.exports = db;
